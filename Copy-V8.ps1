07c5529c626f92d9bd9acdb2e8c75fa3572670f6
aaronamm <theeranitp@gmail.com>
2020-01-19 09:01:42 +0700

    Run all commands in call operation

--- Copy-V8.ps1
+++ Copy-V8.ps1
@@ -1,16 +1,29 @@
 ﻿param(
-    [Parameter(Mandatory = $true)] [string] $SolutionDir,
-    [Parameter(Mandatory = $true)] [string] $TargetDir
+    [Parameter(Mandatory = $true)] [string] $TargetDir,
+    [Parameter(Mandatory = $true)] [string] $WebBinDir
 )
-"SolutionDir $SolutionDir"
+
+$ErrorActionPreference = "Continue"
+$VerbosePreference = "Continue"
+$WarningPreference = "Continue"
+$ProgressPreference = "SilentlyContinue"
+
+"Current dir $PSScriptRoot"
 "TargetDir $TargetDir"
+"WebBinDir $WebBinDir"
+
+return
 
 # https://github.com/projectkudu/kudu/issues/2048
-$WarningPreference = "SilentlyContinue"
 
-$destinationDir = Join-Path -Path $SolutionDir -ChildPath "Orchard.Web/bin/x86"
-"destinationDir $destinationDir"
-New-Item -ItemType Directory $destinationDir -Force -ErrorAction SilentlyContinue | Out-Null
+$destinationDir = Join-Path -Path "$WebBinDir" -ChildPath "x86"
+"Creating $destinationDir directory if not exit"
+New-Item -ItemType Directory -Path "$destinationDir" -Force -ErrorAction Continue
 
-$files = Get-ChildItem -Path $TargetDir -Recurse | Where-Object { $_.FullName -Match "x86.*v8" } 
-$files | Copy-Item -Destination $destinationDir -Force -Verbose
+$files = Get-ChildItem -Path "$TargetDir" -Recurse | Where-Object { "$($_.FullName)" -Match "x86.*v8" } 
+
+"Copying $($files.Length) files to $destinationDir directory"
+$files | ForEach-Object {
+   "Copying $($_.FullName) file to $destinationDir directory"
+    Copy-Item -Path $($_.FullName) -Destination "$destinationDir" -Force -ErrorAction Continue
+}
